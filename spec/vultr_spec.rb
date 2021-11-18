cc1d42c306b39d32993d4ddefac08e62fc24ec2c
Tom Hockett <tom.hockett@gmail.com>
2021-11-18 12:54:59 -0600

    init repo

---
+++ spec/vultr_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vultr do
+  it "has a version number" do
+    expect(Vultr::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
